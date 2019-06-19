61e4fefb69097265853c67900bef7688202aa894
emd5 <elizabeth.mahoney.dao@gmail.com>
2019-06-19 11:20:56 -0700

    fixed post list, add get/set in post

--- src/main/java/com/lizmahoney1/CodeFellowShip/Model/Post.java
+++ src/main/java/com/lizmahoney1/CodeFellowShip/Model/Post.java
@@ -26,4 +26,31 @@ public class Post {
         this.createdAt = createdAt;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+
+
+
 }
